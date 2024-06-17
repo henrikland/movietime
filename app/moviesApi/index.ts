d6c4542a2f0b5a6cd4c73a49260ee1f2236c4af8
Henrik Persson <henrik.h.persson@schibsted.com>
2024-06-17 14:06:35 +0200

    Types and render results

--- app/moviesApi/index.ts
+++ app/moviesApi/index.ts
@@ -1,5 +1,28 @@
 const baseUrl = "https://api.themoviedb.org/3";
 
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+export interface MoviesDBResponse {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
+
 export class MoviesApi {
   private apiKey: string;
 
@@ -7,16 +30,24 @@ export class MoviesApi {
     this.apiKey = apiKey;
   }
 
-  async search(query: string) {
-    const res = await fetch(`${baseUrl}/search/movie?query=${query}`, {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${this.apiKey}`,
-      },
-    });
+  async search(query: string, page: number = 1) {
+    const res = await fetch(
+      `${baseUrl}/search/movie?query=${query}&page=${page}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${this.apiKey}`,
+        },
+      }
+    );
+
     const json = await res.json();
 
-    return json;
+    if (json.success === false) {
+      throw json;
+    }
+
+    return json as MoviesDBResponse;
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,22 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { MoviesApi } from "./moviesApi";
 
-export default async function Home() {
-  const data = await getData();
-  return <div>hej</div>;
+export default async function SearchPage() {
+  const { results } = await getData();
+
+  return (
+    <div>
+      {results.map((movie) => (
+        <p key={movie.id}>{movie.title}</p>
+      ))}
+    </div>
+  );
 }
 
 async function getData() {
   "use server";
   const api = new MoviesApi(process.env.MOVIE_DB_API_KEY ?? "");
   const data = await api.search("jaws");
-  console.log(data);
 
-  return {};
+  return data;
 }
