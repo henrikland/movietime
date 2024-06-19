84f4583418899b58b264843255397258e661bd06
Henrik Persson <henrik.h.persson@schibsted.com>
2024-06-19 20:12:14 +0200

    Header styling

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -3,11 +3,12 @@ import styles from "./header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1>The Movie finder</h1>
+      <h1 className={styles.title}>The Movie finder</h1>
       <nav className={styles.links}>
         <a href="/movies">Movies</a>
         <a href="/tvshows">TV Shows</a>
         <a href="/actors">Actors</a>
+        <button className={styles.button}>Sign In</button>
       </nav>
     </header>
   );
